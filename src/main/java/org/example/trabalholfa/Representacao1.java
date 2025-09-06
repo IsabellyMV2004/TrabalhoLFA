2f3b23d29646af583b0b1294bf82ac8a1b11dee4
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-09-06 17:40:12 -0300

    inicio

--- src/main/java/org/example/trabalholfa/Representacao1.java
+++ src/main/java/org/example/trabalholfa/Representacao1.java
@@ -1,5 +1,20 @@
 package org.example.trabalholfa;
 
+import java.util.regex.*;
 public class Representacao1 {
+
+    public void executar(){
+        String expressao, expr;
+        String regex = "^[0-9a-zA-Z*+|().]+$";
+
+        /*if(expressao.matches(regex)){  //
+            expr = expressao.replace(".", "");
+            expr = "^" + expr + "$";
+
+        }
+        else{ // vai retornar uma mensagem de erro
+
+        }*/
+    }
 
 }
